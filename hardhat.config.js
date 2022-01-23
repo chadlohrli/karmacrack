aa2ab6dbfd52a0332e912af5dd48403e8e727412
chadlohrli <chad.david.lohrli@gmail.com>
2022-01-22 16:41:14 -0800

    Add contracts

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,21 +1,44 @@
 require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-web3");
+require('hardhat-gas-reporter');
+require("@nomiclabs/hardhat-etherscan");
+require('dotenv').config();
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
+
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners();
-
   for (const account of accounts) {
     console.log(account.address);
   }
 });
 
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
+task("balance", "Prints an account's balance")
+  .addParam("account", "The account's address")
+  .setAction(async (taskArgs) => {
+    const account = web3.utils.toChecksumAddress(taskArgs.account);
+    const balance = await web3.eth.getBalance(account);
 
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
+    console.log(web3.utils.fromWei(balance, "ether"), "ETH");
+  });
+
 module.exports = {
-  solidity: "0.8.4",
+    networks: {
+        goerli: {
+            url: process.env.GOERLI_API_KEY,
+            accounts: [process.env.GOERLI_DEPLOY_KEY]
+        },
+    },
+    solidity: {
+        compilers: [
+            {
+                version: "0.8.7",
+                settings: {
+                    optimizer: {
+                      enabled: true,
+                      runs: 200,
+                    },
+                  },
+            },
+        ],   
+    },
 };
